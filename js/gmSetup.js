bd05e95de72f2db145c0defad16b00bdcb8122b5
Niek Rottier <58292770+NiekRottier@users.noreply.github.com>
2020-04-06 21:58:43 +0200

    JS: sends selected gamemode in URL

--- js/gmSetup.js
+++ js/gmSetup.js
@@ -7,6 +7,7 @@ let gmGallery = document.getElementById(`gmGallery`);
 let gm1 = document.getElementById('gm1');
 let gm2 = document.getElementById('gm2');
 let gm3 = document.getElementById('gm3');
+let selectedGm = document.querySelector('[data-selected="1"]').dataset.gamemode;
 
 for (let i=1; i<4; i++){
     gmGallery.addEventListener('click', selectGm);
@@ -16,11 +17,20 @@ function selectGm(e){
     gm1.classList.remove('selectedGm');
     gm2.classList.remove('selectedGm');
     gm3.classList.remove('selectedGm');
+    gm1.dataset.selected = gm2.dataset.selected = gm3.dataset.selected = '0';
+
     if (e.target === gm1 || e.target === gm2 || e.target === gm3){
         e.target.classList.add('selectedGm');
+        e.target.dataset.selected = '1';
+        selectedGm = e.target.dataset.gamemode;
     } else{
         e.target.parentElement.classList.add('selectedGm');
+        e.target.parentElement.dataset.selected = '1';
+        selectedGm = e.target.parentElement.dataset.gamemode;
     }
-
 }
+
+document.getElementById('buttonToGameSetup').addEventListener('click', function redirectToGameSetup(){
+    window.location.replace("gameSetup.html?gm="+selectedGm);
+})
 }
