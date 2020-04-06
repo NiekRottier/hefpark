e4b7f27c82dd9d3245c2a7d480ac8a2c17dbfae7
Niek Rottier <58292770+NiekRottier@users.noreply.github.com>
2020-04-06 13:14:56 +0200

    JS done

---
+++ js/roomcode.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', init);
+
+function init(){
+// GLOBALS ?
+let roomcodeText = document.getElementById(`userRoomcode`);
+let roomcodeSubmit = document.getElementById(`submitRoomcode`);
+
+
+for (let i=0; i<10; i++){ // Add functionality to buttons 9-0
+    let roomcodeButtons = document.getElementById(`button${i}`);
+    roomcodeButtons.addEventListener('click', function typeNumber(){
+        console.log(`Button ${i} has been pressed`);
+        roomcodeText.value = roomcodeText.value + i;
+    })
+}
+
+roomcodeSubmit.addEventListener('click', validateRoomcode);
+
+function validateRoomcode(){
+    if (roomcodeText.value === `123456`){
+        window.location.replace("gmSetup.html");
+    } else {
+        console.log(roomcodeText.value);
+        let errMsg = document.getElementById(`errMsg`);
+        errMsg.innerHTML = `De opgegeven roomcode klopt niet`;
+    }
+}
+}
