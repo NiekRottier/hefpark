49fd3bee989fdae0f63a3dc096ed94da896c0e4d
NiekRottier <niekrottier@gmail.com>
2021-04-15 18:03:27 +0200

    Prevented picking the same playername twice

--- js/gameSetup.js
+++ js/gameSetup.js
@@ -1,13 +1,14 @@
 window.addEventListener('load', init);
 
 // GLOBALS
-let playerName = ["Phineas", "Ferb", "Spongebob", "Patrick", "Octo", "Sandy", "Krabs", "Peppa", "Dora", "Diego",
+let playerNames = ["Phineas", "Ferb", "Spongebob", "Patrick", "Octo", "Sandy", "Krabs", "Peppa", "Dora", "Diego",
     "Aang", "Katara", "Sokka", "Elmo", "Koekiemonster", "Shrek", "Donkey",
     "Po", "Shifu", "Skipper", "Kowalski", "Junior", "Rico", "Gru"];
 let playerAmount;
 let params = new URLSearchParams(location.search);
 let selectedGm = params.get('gm');
-console.log(params.get('gm'));
+console.log("Gamemode = " + params.get('gm'));
+
 let playerSetup = document.getElementById('playerSetup');
 let startButton = document.getElementById('startButton');
 
@@ -20,11 +21,17 @@ function init()
     }
 
     for (let i = 0; i < playerAmount; i++){
-        let x = Math.random()*playerName.length;
+        let x = Math.random()*playerNames.length;
         let pCreate = document.createElement('p');
-        pCreate.innerHTML = playerName[Math.floor(x)];
+        pCreate.innerHTML = playerNames[Math.floor(x)];
         playerSetup.appendChild(pCreate);
-        console.log(playerName[Math.floor(x)])
+        console.log(playerNames[Math.floor(x)])
+
+        // Remove used playername from array
+        let index = playerNames.indexOf(playerNames[Math.floor(x)]);
+        if (index !== -1) {
+            playerNames.splice(index, 1);
+        }
     }
 
     startButton.addEventListener("click", () => window.location.replace("gameProgress.html"))
